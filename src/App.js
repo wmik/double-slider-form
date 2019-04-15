d4fa42f3f04668e339bd9108f9a3780c68b0a37e
wmik <williemik.wmik@gmail.com>
2019-04-15 18:38:14 +0300

    feat: forms
    - Add sign in and sign up forms

--- src/App.js
+++ src/App.js
@@ -97,27 +97,140 @@ const Button = styled.button`
   }
 `;
 
+const Form = styled.form`
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 0 50px;
+  height: 100%;
+  text-align: center;
+`;
+
+const Input = styled.input`
+  background-color: #eee;
+  border: none;
+  padding: 12px 15px;
+  margin: 8px 0;
+  width: 100%;
+`;
+
+const SocialContainer = styled.div`
+  margin: 20px 0;
+`;
+
+const SocialButton = styled.a`
+  border: 1px solid #dddddd;
+  border-radius: 50%;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+  height: 40px;
+  width: 40px;
+`;
+
+const Container = styled.div`
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  position: relative;
+  overflow: hidden;
+  width: 768px;
+  max-width: 100%;
+  min-height: 480px;
+`;
+
+const FormContainer = styled.div`
+  position: absolute;
+  top: 0;
+  height: 100%;
+  transition: all 0.6s ease-in-out;
+`;
+
+const SignUpFormContainer = styled(FormContainer)`
+  left: 0;
+  width: 50%;
+  opacity: 0;
+  z-index: 1;
+`;
+
+const SignInFormContainer = styled(FormContainer)`
+  left: 0;
+  width: 50%;
+  z-index: 2;
+`;
+
+function SignUpForm() {
+  return (
+    <SignUpFormContainer>
+      <Form>
+        <Header>Create account</Header>
+        <SocialContainer>
+          <SocialButton>
+            <i className="fab fa-facebook-f" />
+          </SocialButton>
+          <span>or use your email for registration</span>
+          <Input type="text" placeholder="name" />
+          <Input type="email" placeholder="email" />
+          <Input type="password" placeholder="password" />
+          <Button>Sign up</Button>
+        </SocialContainer>
+      </Form>
+    </SignUpFormContainer>
+  );
+}
+
+function SignInForm() {
+  return (
+    <SignInFormContainer>
+      <Form>
+        <Header>Create account</Header>
+        <SocialContainer>
+          <SocialButton>
+            <i className="fab fa-facebook-f" />
+          </SocialButton>
+          <span>or use your account</span>
+          <Input type="email" placeholder="email" />
+          <Input type="password" placeholder="password" />
+          <a>forgot password</a>
+          <Button>Sign up</Button>
+        </SocialContainer>
+      </Form>
+    </SignInFormContainer>
+  );
+}
+
+function OverlayComponent() {
+  return (
+    <OverlayContainer>
+      <Overlay>
+        <OverlayLeft>
+          <Header>Welcome back</Header>
+          <Text>
+            To keep connected with us please login with your personal info
+          </Text>
+          <Button>Sign In</Button>
+        </OverlayLeft>
+        <OverlayRight>
+          <Header>Hello, Friend!</Header>
+          <Text>Enter your personal details and start journey with us</Text>
+          <Button ghost>Sign In</Button>
+        </OverlayRight>
+      </Overlay>
+    </OverlayContainer>
+  );
+}
+
 function App() {
   return (
-    <React.Fragment>
+    <Container>
       <GlobalStyle />
-      <OverlayContainer>
-        <Overlay>
-          <OverlayLeft>
-            <Header>Welcome back</Header>
-            <Text>
-              To keep connected with us please login with your personal info
-            </Text>
-            <Button>Sign In</Button>
-          </OverlayLeft>
-          <OverlayRight>
-            <Header>Hello, Friend!</Header>
-            <Text>Enter your personal details and start journey with us</Text>
-            <Button ghost>Sign Up</Button>
-          </OverlayRight>
-        </Overlay>
-      </OverlayContainer>
-    </React.Fragment>
+      <SignUpForm />
+      <SignInForm />
+      <OverlayComponent />
+    </Container>
   );
 }
 
